5cd4816b37f70ced3756e66cf3e2fc0382f19165
derkdts <160583121+derkdts@users.noreply.github.com>
2025-03-24 17:56:22 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,18 +6,18 @@ def main():
   st.title("Чек-лист по городам")
  
 
-  # Добавляем JavaScript для изменения заголовка страницы
-components.html(
-"""
-<script>
-document.title = 'Чек-лист по городам';
-</script>
-""",
-height=0,
-)
+  # JavaScript для изменения заголовка страницы
+  components.html(
+  """
+  <script>
+  document.title = 'Чек-лист по городам';
+  </script>
+  """,
+  height=0,
+  )
  
 
-checklist_items = {
+  checklist_items = {
   "checkbox_A": "A, B - Алматы",
   "checkbox_Z": "Z - Астана",
   "checkbox_X": "Х - Шымкент",
@@ -30,40 +30,48 @@ checklist_items = {
   "checkbox_M": "M - Караганда",
   "checkbox_P": "P - Костанай",
   "checkbox_H": "H - Тараз",
-  "checkbox_T": "T - Петропавловск"
-}
+  "checkbox_T": "T - Петропавловск",
+  }
  
 
+  # Инициализация состояния
 if 'checkbox_states' not in st.session_state:
   st.session_state.checkbox_states = {key: False for key in checklist_items}
  
 
+  # Функция сброса прогресса
 def reset_progress():
-  for key in st.session_state.checkbox_states:
-   st.session_state.checkbox_states[key] = False
+  st.session_state.checkbox_states = {key: False for key in checklist_items}
  
 
+  # Кнопка сброса
   st.button("Сбросить прогресс", on_click=reset_progress)
  
 
-cols = st.columns(3)
-i = 0
-for key, label in checklist_items.items():
-  col_num = i % 3
-  st.session_state.checkbox_states[key] = cols[col_num].checkbox(label, key=key, value=st.session_state.checkbox_states[key])
-  i+=1
+  # Создание колонок
+  cols = st.columns(3)
  
 
-  # Вычисляем и отображаем прогресс только один раз
+  # Создание чекбоксов
+for i, (key, label) in enumerate(checklist_items.items()):
+  col_num = i % 3
+  st.session_state.checkbox_states[key] = cols[col_num].checkbox(
+  label, key=key, value=st.session_state.checkbox_states[key]
+  )
+ 
+
+  # Вычисление прогресса
   completed_count = sum(st.session_state.checkbox_states.values())
   total_count = len(checklist_items)
  
 
+  # Отображение прогресса
   progress_percent = int((completed_count / total_count) * 100)
   st.progress(progress_percent)
   st.write(f"Выполнено: {progress_percent}% ({completed_count}/{total_count})")
  
 
+  # Сообщение об успехе
 if completed_count == total_count:
   st.success("Поздравляем! Все пункты выполнены!")
  

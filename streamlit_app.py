8dfb2b5c09f6bda2eda1286e6a700b9a86ff7101
derkdts <160583121+derkdts@users.noreply.github.com>
2025-03-24 17:38:14 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,18 +6,18 @@ def main():
   st.title("Чек-лист по городам")
  
 
- # Добавляем JavaScript для изменения заголовка страницы
-components.html(
-"""
-<script>
-document.title = 'Чек-лист по городам';
-</script>
-""",
-height=0,
-)
+  # Добавляем JavaScript для изменения заголовка страницы
+  components.html(
+  """
+  <script>
+  document.title = 'Чек-лист по городам';
+  </script>
+  """,
+  height=0,
+  )
  
 
-checklist_items = {
+  checklist_items = {
   "checkbox_A": "A, B - Алматы",
   "checkbox_Z": "Z - Астана",
   "checkbox_X": "Х - Шымкент",
@@ -30,7 +30,7 @@ checklist_items = {
   "checkbox_M": "M - Караганда",
   "checkbox_P": "P - Костанай",
   "checkbox_H": "H - Тараз",
-  "checkbox_T": "Т - Петропавловск"
+  "checkbox_T": "T - Петропавловск"
   }
  
 
@@ -40,26 +40,27 @@ if 'checkbox_states' not in st.session_state:
 
 def reset_progress():
   st.session_state.checkbox_states = {key: False for key in checklist_items}
+  st.rerun() # Добавляем st.rerun()
  
 
-st.button("Сбросить прогресс", on_click=reset_progress)
+  st.button("Сбросить прогресс", on_click=reset_progress)
  
 
-cols = st.columns(3)
+сols = st.columns(3)
 i = 0
 for key, label in checklist_items.items():
   col_num = i % 3
-  st.session_state.checkbox_states[key] = cols[col_num].checkbox(label, key=key)
+  st.session_state.checkbox_states[key] = cols[col_num].checkbox(label, key=key, value=st.session_state.checkbox_states[key]) # Добавляем value
   i+=1
  
 
-completed_count = sum(st.session_state.checkbox_states.values())
-total_count = len(checklist_items)
+  completed_count = sum(st.session_state.checkbox_states.values())
+  total_count = len(checklist_items)
  
 
-progress_percent = int((completed_count / total_count) * 100)
-st.progress(progress_percent)
-st.write(f"Выполнено: {progress_percent}% ({completed_count}/{total_count})")
+  progress_percent = int((completed_count / total_count) * 100)
+  st.progress(progress_percent)
+  st.write(f"Выполнено: {progress_percent}% ({completed_count}/{total_count})")
  
 
 if completed_count == total_count:
